e65e67da480a71d10921cb78faa3f1566d846bdd
AryaSriva <87484641+AryaSriva@users.noreply.github.com>
2024-07-05 16:29:00 -0400

    added model and flask files

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, render_template
+import numpy as np
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open("model.pkl", "rb"))
+
+
+@app.route("/")  # home root
+def home():
+    return render_template("index.html")
+
+
+@app.route("/predict", methods=["POST"])  # prediction post method for submission
+def predict():
+    # predict price charged based on user inputted distance travelled and cost of trip
+    args = [int(x) for x in request.form.values()]
+    prediction = model.predict([np.array(args)])
+    output = round(prediction[0], 2)
+    return render_template(
+        "index.html", prediction="You can expect to charge ${}".format(output)
+    )
+
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
